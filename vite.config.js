ee7c8c821e91e5b502898a02c9e7cbf6a95bdfd9
Paulo Vitor <paulonovaes@id.uff.br>
2023-06-07 19:33:32 -0300

    estrutura para sockets

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
-import express from 'express';
-import http from "http";
-import url from "url";
-import path from "path";
-import { Server } from "socket.io";
-import { createServer as createViteServer } from 'vite';
+import { defineConfig } from "vite"
 
-async function createServer() {
-    const app = express()
-    // Create Vite server in middleware mode
-    const vite = await createViteServer({
-        server: { middlewareMode: true },
-        appType: 'custom', // don't include Vite's default HTML handling middlewares,
-    })
-    // Use vite's connect instance as middleware
-    app.use(vite.middlewares)
-
+export default defineConfig({
     
-    
-}
-
-createServer()
+    build: {
+        chunkSizeWarningLimit: 1600,
+    }
+})
