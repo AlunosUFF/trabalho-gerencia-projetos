7a09c965948d774d0d230ccfe079b41af0703154
Rafaela Peçanha <rafaela.pecanha.si@gmail.com>
2023-06-08 00:39:50 -0300

    feat: iniciando lógica de destacar o continente

--- src/view/LocalizadorContinente.ts
+++ src/view/LocalizadorContinente.ts
@@ -1,15 +1,36 @@
+import { Territory } from "../model/Territory";
+
 export default class LocalizadorContinente extends Phaser.GameObjects.Container{
 
-    constructor(data: {scene: Phaser.Scene, continent}) {
-        let {scene, continent} = data;
+    constructor(data: {scene: Phaser.Scene, continent: any, territorio:any}) {
+        let {scene, continent,territorio} = data;
 
         let localizador = new Phaser.GameObjects.Sprite(scene,continent.textX,continent.textY,'localizador')
         let continentText = new Phaser.GameObjects.Text(scene, 0,0, 
             `${continent.name} +${continent.totality}`,{wordWrap:{width:120}})
         console.log(localizador)
         super(scene, continent.x, continent.y, [localizador,continentText])
+        // console.log(continent.id)
+        console.log(territorio[0])
+        localizador.setInteractive({useHandCursor: true})
+        localizador.on("pointerdown", (pointer, objeto)=>{
+            // if(continent.id===territorio.continent){
+            //     alert("oi")
+            // }
 
-        console.log(this.scene)
+            territorio.forEach(objectId=>{
+                console.log(objectId.name)
+            })
+
+        //  continent.filter(continent.id===territorio.continent).forEach(object=>{
+        //      object.setVisible(true);
+ 
+        //     })
+        //     // console.log(continent.id)
+        })
+        
         this.scene.add.existing(this)
+
+        
     }
 }
