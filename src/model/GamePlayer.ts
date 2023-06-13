b031d025023936cd0e1adc82edb69d4d21d1a1d2
Paulo Vitor <paulonovaes@id.uff.br>
2023-06-12 23:42:42 -0300

    Revert "usando pattern enums"
    This reverts commit ead721242fa27fd9618e16a130c61c1194c3f954.

--- src/model/GamePlayer.ts
+++ src/model/GamePlayer.ts
@@ -3,7 +3,7 @@ import Objective from "./Objective";
 import PlayerType, { Player } from "./Player";
 import { Territory } from "./Territory";
 
-export enum playerCOLORS {
+export enum playerCOLORS  {
     'black' = 0x4f4f4d,
     'green' = 0x13a95b,
     'yellow' = 0xe9ae02,
@@ -12,89 +12,46 @@ export enum playerCOLORS {
     'red' = 0xec3829,
 }
 
-export enum Continent {
-    All = 0,
-    SouthAmerica = 1,
-    Asia = 2,
-    Oceania = 3,
-    Africa = 4,
-    NorthAmerica = 5,
-    Europe = 6
+export interface Placeble{
+    "all": number,
+    "south-america": number,
+    "asia": number,
+    "oceania": number,
+    "africa": number,
+    "north-america": number,
+    "europe": number
 }
 
-export interface Placeble {
-    [key: string]: number;
-}
+// export interface Placed {
+//     "all": number
+// }
 
-export function getContinentFromString(continentString: string): Continent {
-    switch (continentString) {
-      case 'south-america':
-        return Continent.SouthAmerica;
-      case 'asia':
-        return Continent.Asia;
-      case 'oceania':
-        return Continent.Oceania;
-      case 'africa':
-        return Continent.Africa;
-      case 'north-america':
-        return Continent.NorthAmerica;
-      case 'europe':
-        return Continent.Europe;
-      case 'all':
-        return Continent.All;
-      default:
-        console.error("erro ao converter string para continent");
-        throw new Error(`Continent inválido: ${continentString}`);
-    }
-  }
-  
-export function getStringFromContinent(continent: Continent): string {
-    switch (continent) {
-      case Continent.SouthAmerica:
-        return 'south-america';
-      case Continent.Asia:
-        return 'asia';
-      case Continent.Oceania:
-        return 'oceania';
-      case Continent.Africa:
-        return 'africa';
-      case Continent.NorthAmerica:
-        return 'north-america';
-      case Continent.Europe:
-        return 'europe';
-      case Continent.All:
-        return 'all';
-      default:
-        console.error("erro ao converter continent para string");
-        throw new Error(`Continent inválido: ${continent}`);
-    }
-}
-export class GamePlayer extends Player {
-
+export class GamePlayer extends Player{
+    
     // public cards: Array<Car
     public color: number;
     public totalArmies: number;
     public totalTerritories: number;
     public playerText: Phaser.GameObjects.Text;
+    public armies;
     public destroyed = false;
-
+    
     public placed: Placeble = {
-        [Continent.All]: 0,
-        [Continent.SouthAmerica]: 0,
-        [Continent.Asia]: 0,
-        [Continent.Oceania]: 0,
-        [Continent.Africa]: 0,
-        [Continent.NorthAmerica]: 0,
-        [Continent.Europe]: 0
+        "all":0, "south-america":0,    
+        "asia": 0,
+        "oceania": 0,
+        "africa": 0,
+        "north-america": 0,
+        "europe": 0
     }
     public placeble: Placeble = {
-        [Continent.All]: 0,
-        [Continent.SouthAmerica]: 0,
-        [Continent.Asia]: 0,
-        [Continent.Oceania]: 0,
-        [Continent.Africa]: 0,
-        [Continent.NorthAmerica]: 0,
-        [Continent.Europe]: 0
+        "all":0, 
+        "south-america":0,    
+        "asia": 0,
+        "oceania": 0,
+        "africa": 0,
+        "north-america": 0,
+        "europe": 0
     }
     public gainedTerritory = false;
     public hand: number[] = []
@@ -104,95 +61,89 @@ export class GamePlayer extends Player {
 
 
     // public ia: boolean;
-    constructor(data: PlayerType, color: number, warMatch: WarMatch, ia: boolean, totalArmies: number, totalTerritories: number, playerText: Phaser.GameObjects.Text,
-        objective: Objective, aimer: GamePlayer) {
+    constructor(data:PlayerType, color: number, warMatch: WarMatch, ia: boolean) {
         super(data);
         this.color = color;
         this.warMatch = warMatch;
         this.ia = data.ia;
-        this.totalArmies = totalArmies;
-        this.totalTerritories = totalTerritories;
-        this.playerText = playerText;
-        this.objective = objective;
-        this.aimer = aimer;
     }
 
-    destroyPlayerText() {
+    destroyPlayerText(){
         this.playerText.destroy()
     }
 
-    showGamePlayer(x: number, y: number, currentPlayerId: number, scene: Phaser.Scene) {
+    showGamePlayer(x: number, y: number, currentPlayerId:number, scene:Phaser.Scene) {
         let isCurrentPlayer = currentPlayerId === this.id;
-        this.playerText = scene.add.text(x, y, `${this.name} ⚒: ${this.totalArmies} ⦻: ${this.totalTerritories}`)
-            .setColor(`#${this.color.toString(16)}`)
-        if (isCurrentPlayer) {
+        this.playerText = scene.add.text(x,y,`${this.name} ⚒: ${this.totalArmies} ⦻: ${this.totalTerritories}`)
+        .setColor(`#${this.color.toString(16)}`)
+        if(isCurrentPlayer){
             this.playerText.setBackgroundColor("#ffffff55")
         }
-        this.playerText.setInteractive({ useHandCursor: true });
+        this.playerText.setInteractive( { useHandCursor: true  } );
 
-        this.playerText.on("pointerdown", () => {
+        this.playerText.on("pointerdown",()=>{
             //Mostrar os dados do jog, etc
         })
     }
 
-    isCurrentPlayer() {
+    isCurrentPlayer(){
         return this.id === this.warMatch.turn.getCurrentPlayerId()
     }
 
-    setPlaceble(type: string, quantity: number) {
-        let continent = getContinentFromString(type)
-        this.placeble[continent] = quantity;
+    setPlaceble(type:string, quantity:number){
+        this.placeble[type] = quantity;
     }
 
-    addPlaceble(type: string, quantity: number) {
-        let continent = getContinentFromString(type)
-        this.placeble[continent] += quantity;
+    addPlaceble(type:string, quantity:number){
+        this.placeble[type] += quantity;
     }
 
-    placeArmie(type: string, quantity: number) {
-        let continent = getContinentFromString(type)
-        if (this.hasArmiesToPlace()) {
-            this.placeble[continent] -= quantity;
-            this.placed[continent] += quantity;
+    placeArmie(type:string, quantity:number){
+        if(this.hasArmiesToPlace()){
+            this.placeble[type] -= quantity;
+            this.placed[type] += quantity;
         }
     }
 
-    updateTotalTerritories() {
+    updateTotalTerritories(){
         this.totalTerritories = this.warMatch.getPlayerTerritories(this).length;
     }
 
-    unplaceArmie(type: string, quantity: number) {
-        var continent = getContinentFromString(type)
-        this.placeble[continent] += quantity;
-        this.placed[continent] -= quantity;
+    unplaceArmie(type:string, quantity:number){
+        // if(this.placeble)
+        this.placeble[type] += quantity;
+        this.placed[type] -= quantity;
     }
 
     clearPlaced() {
-        Object.keys(this.placed).forEach(key => {
-            const place = Number(key);
-            this.placeble[place] = 0;
-            this.placed[place] = 0;
+        Object.keys(this.placed).forEach(key =>{
+            this.placeble[key] = 0;
+            this.placed[key] = 0;
         })
     }
 
-    hasArmiesToPlace() {
+    hasArmiesToPlace(){
         let placesToPlace = Object.keys(this.placeble).filter(key => {
             const place = Number(key);
             return this.placeble[place] > 0
         }).length
-
+        
         return placesToPlace > 0
     }
 
-    isOwner(territory: Territory) {
+    isOwner(territory: Territory){
         return this.id === territory.owner?.id
     }
 
-    hasBeenDestroyed() {
+    hasBeenDestroyed(){
         return this.totalTerritories === 0
     }
 
-    resetPlaces() {
+    // resetPlaced(){
+    //     this.placed.all = 0
+    // }
 
+    resetPlaces(){
+        
     }
 }
