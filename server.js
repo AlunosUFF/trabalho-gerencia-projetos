8408cbf2cafe438aa42ad9875cce122b49ea347a
Paulo Vitor <paulonovaes@id.uff.br>
2023-06-15 01:33:57 -0300

    adicionando server

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+import path from 'path';
+import fs from 'fs'
+import { fileURLToPath } from 'url'
+
+const app = express();
+const server = http.createServer(app);
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// Configurar o diretório onde estão os arquivos estáticos
+const staticPath = path.join(__dirname, 'dist');
+app.use(express.static(staticPath, { extensions: ['html'] }));
+
+// Rota para servir o arquivo index.html
+app.get('/', (_, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
+
+const io = new SocketIOServer(server);
+
+const port = process.env.PORT || 3000;
+
+// Configurar o Socket.IO
+io.on('connection', (socket) => {
+  console.log('Novo cliente conectado');
+
+  // Lidar com eventos do Socket.IO aqui
+});
+
+// Inicia o servidor
+server.listen(port, () => {
+  console.log(`Servidor iniciado em http://localhost:${port}`);
+});
+
+export default app;
